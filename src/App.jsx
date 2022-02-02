b58b87326e14e3dcedd03d3dca867f659672178a
Rahul Kumar Jhamtani <77382243+rahulkumarjhamtani@users.noreply.github.com>
2022-02-02 12:11:18 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ const App = () => {
       projectID="4175a0e2-a95c-49cb-bbb3-40056d23fc05"
       userName="Rahul"
       userSecret="1234"
-      renderCharFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+      renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
     />
   );
 };
