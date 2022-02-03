5778015787b2eb67f621851c6509478ca1ed519d
Rahul Kumar Jhamtani <77382243+rahulkumarjhamtani@users.noreply.github.com>
2022-02-03 09:51:52 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,24 @@
 import { ChatEngine } from 'react-chat-engine';
 
 import ChatFeed from './components/ChatFeed';
-// import LoginForm from './components/LoginForm';
+import LoginForm from './components/LoginForm';
 
 import './App.css';
 
-// const projectID = '4175a0e2-a95c-49cb-bbb3-40056d23fc05';
+const projectID = '4175a0e2-a95c-49cb-bbb3-40056d23fc05';
 
 const App = () => {
-  // if (!localStorage.getItem('username')) return <LoginForm />;
+  if (!localStorage.getItem('username')) return <LoginForm />;
 
   return (
     <ChatEngine
       offset={6}
       height="100vh"
-      projectID="4175a0e2-a95c-49cb-bbb3-40056d23fc05"
-      userName="Rahul"
-      userSecret="1234"
+      projectID={projectID}
+      userName={localStorage.getItem('username')}
+      userSecret={localStorage.getItem('password')}
       renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+      onNewMessage={() => new Audio('https://chat-engine-assets.s3.amazonaws.com/click.mp3').play()}
     />
   );
 };
